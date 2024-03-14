d4df0344955a5e6807cdf5990a4c5dc66450337b
Mohammadhossein Misaghpour <misagh1999@gamil.com>
2024-03-14 17:11:49 +0330

    implemented: display article image with coil

--- app/src/main/java/app/misaghpour/news/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/app/misaghpour/news/ui/screens/home/HomeScreen.kt
@@ -2,17 +2,26 @@ package app.misaghpour.news.ui.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import app.misaghpour.news.R
 import app.misaghpour.news.model.Article
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun HomeScreen(
@@ -22,13 +31,15 @@ fun HomeScreen(
     val uiState = viewModel.newsUiState
 
     Column {
-        when(uiState){
+        when (uiState) {
             is NewsUiState.Loading -> {
                 Text(text = "Loading")
             }
+
             is NewsUiState.Success -> {
                 ArticleList(articles = uiState.articles)
             }
+
             is NewsUiState.Error -> {
                 Text(text = "error")
             }
@@ -40,12 +51,33 @@ fun HomeScreen(
 @Composable
 fun ArticleList(
     articles: List<Article>,
-    modifier: Modifier = Modifier) {
+    modifier: Modifier = Modifier
+) {
     LazyColumn {
-        items(articles){article ->
+        items(articles) { article ->
             Column {
-                Text(text = article.title ?: "---", style = TextStyle(fontWeight = FontWeight.Bold))
-                Text(text = article.description ?: "****")
+                AsyncImage(
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(article.urlToImage)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = article.title ?: "",
+                    error = painterResource(R.drawable.ic_broken_image),
+                    placeholder = painterResource(R.drawable.loading_img),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxWidth()
+
+                )
+                Text(
+                    text = article.title ?: "---",
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 16.sp),
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                )
+                Text(
+                    text = article.description ?: "****",
+                    style = TextStyle(fontSize = 14.sp),
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                )
                 Spacer(modifier = Modifier.height(8.dp))
             }
         }
